ff506e52c5d8e244094aa4fb27ba4ec5ea517f19
Patricia Ramirez Cifuentes <ramirezcpatricia1@gmail.com>
2022-09-16 20:49:35 -0500

    variable name updated

--- list_comprehension2.py
+++ list_comprehension2.py
@@ -1,7 +1,7 @@
 def run():  
-    squares =[i for i in range(1,100000) if i % 4 == 0  if i % 6 == 0 if i % 9 == 0]
+    dividends =[i for i in range(1,100000) if i % 4 == 0  if i % 6 == 0 if i % 9 == 0]
 
-    print(squares)
+    print(dividends)
 
 
 if __name__ == '__main__':
